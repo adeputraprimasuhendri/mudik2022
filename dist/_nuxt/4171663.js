94839fe364bca5d4f9246cf32f37776a2e053797
adeputraprimasuhendri <ideatechnologyindonesia@gmail.com>
2022-04-28 15:19:54 +0700

    change base url

---
+++ dist/_nuxt/4171663.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{189:function(n,t,e){"use strict";var c=e(46),component=Object(c.a)({},(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("Nuxt")],1)}),[],!1,null,null,null);t.a=component.exports},190:function(n,t,e){e(191),n.exports=e(192)}},[[190,10,1,11]]]);
