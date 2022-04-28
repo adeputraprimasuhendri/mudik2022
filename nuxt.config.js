88f2b4c1f0cb0726ba07eb7371e74be059072e30
adeputraprimasuhendri <ideatechnologyindonesia@gmail.com>
2022-04-28 17:05:24 +0700

    change url

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   css: [
   ],
   axios: {
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://appsworkspace.com'
   },
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
